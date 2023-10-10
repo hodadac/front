1327a22bb1c09cc01ee901c2fca99e7c6a25ded5
han <dusrb0803@naver.com>
2023-10-10 19:56:47 +0900

    10/10 ip

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://127.0.0.1:8080',
+            target: 'http://43.202.196.83:8080',
             changeOrigin: true,
         })
     );
